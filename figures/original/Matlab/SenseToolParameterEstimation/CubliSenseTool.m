65627764ae228c5ab2c7c23a9ccf1ea311f8f530
Noelia <noelia.va.3003@gmail.com>
2016-04-03 19:01:40 +0200

    Add some of the controller section

--- figures/original/Matlab/SenseToolParameterEstimation/CubliSenseTool.m
+++ figures/original/Matlab/SenseToolParameterEstimation/CubliSenseTool.m
@@ -3,6 +3,7 @@ close all
 clc
 
 %% Cubli model parameters
+
 T_m=0.005;
 K=0.5;
 J_w=0.601e-3;
@@ -10,14 +11,15 @@ J_f=(2.8e-3);
 B_w=17.03e-6;
 B_f=6.08e-3;
 m_w=0.222;
-m_f=0.354;
+m_f=0.77-m_w;%0.354;
 g=9.82;
 l_w=0.093;
-l_f=0.076;
+l_f=0.08498;%0.076;
 
 %%
+
 %for the last part of the signal:
-pendulumData = csvread('pendulum.CSV', 709, 0, [ 709 0 1300 1 ]); %row 709 to 1300
+pendulumData = csvread('PRINT_03.CSV'); %row 709 to 1300
 %simulation time: 5.91
 %initial condition: 0.1487
 
@@ -27,11 +29,9 @@ pendulumData = csvread('pendulum.CSV', 709, 0, [ 709 0 1300 1 ]); %row 709 to 13
 %initial condition: -pi/4
 
 %for the last part of the signal:
-t = pendulumData(:,1)'+2.91;
-
+t = pendulumData(:,1)'+5;
 %for whole signal:
 %t = pendulumData(:,1)'+10-.72;
-
 
 Yvolt = pendulumData(:,2);
 
@@ -39,22 +39,19 @@ u = zeros(size(Yvolt));
 uin = [ t' u ];
 
 % Data from Volt to rads
-Vmin=0.004316370106762;
-Vmax=0.466679601990048;
-equVolt=0.215324792013310;  
-middleVolt=0.235497986048405;
-
-offsetVolt = middleVolt-equVolt+.004;
-resRad = (pi/2)/(Vmax-Vmin);
-y = (Yvolt - middleVolt + offsetVolt)*resRad;
+Vmin = 0.4708;%   mean(voltage( 667:1243,1)) %voltage mean from 3.3 to 6.2 in time
+Vmax = 0.9513;%   mean(voltage(1336:2000,1)) %voltage mean from 6.7 to 10 in time
+equVolt = 0.6945;%mean(voltage(   1: 591,1)) %voltage mean from 0 to 2.9 in time
+resRad = (1.5769)/(Vmax-Vmin);
+y = (Yvolt - equVolt)*resRad-0.005;
 
 scatter(t',y)
 
 %%
+
 par0 = [ B_f J_f ];% m_f l_f ];
 
 Ynew = simCubli(u,t,par0);
-
 %hold on;
 %plot(t, Ynew);
 %hold off;
@@ -63,11 +60,9 @@ save measCubli t u y %creating measTestName
 
 process = 'Cubli';
 
-
 %%
 
 run mainest.m
-
 
 %% PLOTTING IT PRETTILY :)
 
@@ -75,9 +70,9 @@ a = get(findall(gcf, 'Type', 'line', 'color', '[0.8500 0.3250 0.0980]'));
 Ynew = a.YData;
 
 figure;
-scatter(t', y, 'r')
+plot(t', y, 'b')
 hold on
-plot(t', Ynew, 'b', 'LineWidth', 1.4)
+plot(t', Ynew, 'r', 'LineWidth', 1.4)
 grid on, grid minor;
 set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6)
 
@@ -86,7 +81,3 @@ xlabel('Time (s)')
 ylabel('Angle (rad)')
 
 hold off
-
-
-
-
