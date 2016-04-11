88ebe052c8c62497c60817a81d4188cb9efdbae1
AodhAnd <julbrehin@gmail.com>
2016-04-11 22:00:27 +0200

    Updated bode plots with correct controller

--- figures/original/Matlab/bodePrewarpVsNoPrewarpVsContinuous/discreteOfModel.m
+++ figures/original/Matlab/bodePrewarpVsNoPrewarpVsContinuous/discreteOfModel.m
@@ -4,13 +4,14 @@ clc;
 s=tf('s');
 T=1/100;%1/100;
 
-D=-4583.5*(s+9.483)*(s+1.599)/((s-5.539)*(s+100)*(s+200))/0.62737;
+D=-4583.5*(s+9.483)*(s+1.599)/((s-5.539)*(s+100)*(s+200))%/0.62737;
 G=(-J_w*s^2)/((J_w*s^2+B_w*s)*((J_f+m_w*l_w^2)*s^2+B_f*s-(m_w*l_w+m_f*l_f)*g+((B_w*J_w*s^3)/(J_w*s^2+B_w*s))));
 G_reduced=minreal(G);
 
 Gz=c2d(G_reduced,T,'tustin');
 
 Dz=c2d(D,T,'tustin')
+% Look at wp = 38.351 or 33.27356999 or in between
 opts = c2dOptions('Method', 'tustin', 'PrewarpFrequency', 33.5);
 Dz2 = c2d(D,T,opts)
 
