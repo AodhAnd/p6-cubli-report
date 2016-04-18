6f1b66196c46ae7efe23da7f5518b9fe475681fc
Noelia <noelia.va.3003@gmail.com>
2016-04-18 17:29:51 +0200

    Write on the conclusions of the controller and add pictures

--- figures/original/Matlab/SimController/randomController.m
+++ figures/original/Matlab/SimController/randomController.m
@@ -1,3 +1,7 @@
+clear all;
+close all;
+clc;
+
 %% Cubli model parameters
 J_w=0.601e-3;
 J_f=5.6e-3;
@@ -9,6 +13,10 @@ g=9.82;
 l_w=0.093;
 l_f=0.08498;%0.076;
 
+% Disturbance data
+disturb=0.55;   % Disturbance torque value in Nm
+t_dist=0.01;    % Duration of the disturbance
+
 %% Nonlinearized model vs linearized one
 warning('off');
 sim('randomControllerSim2.slx');
@@ -17,18 +25,41 @@ warning('on');
 %% Angular Position Plot
 figure(1);
 plot(position,'linewidth',1.2);
-title('Closed Loop Response with the Controller')
+hold on;
+plot(position1,'linewidth',1.2);
+title('Closed Loop Response in Position')
 xlabel('Time (s)');
 ylabel('Angular position (rad)');
 grid on, grid minor;
 set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6);
-xlim([0 0.8]);
+legend('With saturation','Without saturation');
+xlim([0 1.3]);
+hold off;
 
 figure(2);
 plot(torque,'linewidth',1.2);
-title('Closed Loop Response with the Controller')
+hold on;
+plot(torque1,'linewidth',1.2);
+title('Closed Loop Response in Torque')
 xlabel('Time (s)');
 ylabel('Torque (Nm)');
 grid on, grid minor;
 set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6);
-xlim([0 0.8]);
+legend('With saturation','Without saturation');
+xlim([0 1.3]);
+ylim([-0.05 0.17]);
+hold off;
+
+figure(3);
+plot(wheel,'linewidth',1.2);
+hold on;
+plot(wheel1,'linewidth',1.2);
+title('Closed Loop Response in the Wheel')
+xlabel('Time (s)');
+ylabel('Angular velocity (rad/s)');
+grid on, grid minor;
+set(gca,'GridLineStyle',':', 'GridColor', 'k', 'GridAlpha', .6);
+legend('With saturation','Without saturation');
+xlim([0 1.3]);
+ylim([-2 18]);
+hold off;
